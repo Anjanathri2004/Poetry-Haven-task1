f48a520951fb7b86d7a733b86765c9f1e495d9a6
Anjanathri2004 <161334855+Anjanathri2004@users.noreply.github.com>
2024-08-28 19:35:14 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function logIn() {
+    alert("Log In button clicked!");
+}
+
+function createAccount() {
+    alert("Create Account button clicked!");
+}
+
+function subscribe() {
+    alert("Subscribe Now button clicked!");
+}
